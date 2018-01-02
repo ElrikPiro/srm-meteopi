498e8f5623e988d98cab7675706a7bd431f56373
David Baselga-Masia <dabama1@upv.es>
2018-01-02 12:43:19 +0000

    Shaking example

---
+++ src/shake_me.py
@@ -0,0 +1,46 @@
+from sense_hat import SenseHat
+from time import sleep
+from random import randint
+
+sense = SenseHat()
+sense.clear()
+
+red = (255,0,0)
+blue = (0,0,255)
+green = (0,255,0)
+white = (255,255,255)
+yellow = (255,255,0)
+orange = (255,127,0)
+violet = (139,0,255)
+
+def random_color():
+    random_red = randint(0,255)
+    random_green = randint(0,255)
+    random_blue = randint(0,255)
+    return (random_red,random_green,random_blue)
+
+def spain_flag():
+    pixels = [
+            red,red,red,green,red,red,red,red,
+            red,red,red,red,red,red,red,red,
+            yellow,yellow,yellow,yellow,yellow,yellow,yellow,yellow,
+            yellow,yellow,yellow,yellow,yellow,yellow,yellow,yellow,
+            yellow,yellow,yellow,yellow,yellow,yellow,yellow,blue,
+            yellow,yellow,yellow,yellow,yellow,yellow,yellow,yellow,
+            red,red,red,red,red,red,red,red,
+            red,red,red,red,red,red,red,red
+            ]
+    sense.set_pixels(pixels)
+    return;
+
+while True:
+    a = sense.get_accelerometer_raw()
+    
+    x = abs(a['x'])
+    y = abs(a['y'])
+    z = abs(a['z'])
+    
+    if x > 1 or y > 1 or z > 1:
+        sense.show_letter("!",red)
+    else:
+        sense.clear()
